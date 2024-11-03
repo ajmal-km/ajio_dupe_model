09e9c2728f621d8e164038be4b035066b7652a34
ajmal-km <ajmalkm125@gmail.com>
2024-11-03 20:53:35 +0530

    edited

--- lib/view/cart_screen/cart_screen.dart
+++ lib/view/cart_screen/cart_screen.dart
@@ -4,6 +4,7 @@ import 'package:ajio_dupe_model/view/cart_screen/widgets/cart_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -18,6 +19,7 @@ class _CartScreenState extends State<CartScreen> {
     WidgetsBinding.instance.addPostFrameCallback(
       (timeStamp) {
         context.read<CartScreenController>().getCartedProduct();
+        context.read<CartScreenController>().calculateTotalPrice();
       },
     );
     super.initState();
@@ -74,6 +76,117 @@ class _CartScreenState extends State<CartScreen> {
           },
         ),
       ),
+      bottomNavigationBar: Container(
+        height: 67,
+        padding: EdgeInsets.symmetric(horizontal: 18),
+        color: ColorConstants.white,
+        child: Row(
+          children: <Widget>[
+            Text(
+              "Total : ₹ ${cartProvider.totalPrice}",
+              style: TextStyle(
+                color: ColorConstants.black,
+                fontSize: 19,
+                fontWeight: FontWeight.w700,
+                letterSpacing: -0.6,
+              ),
+            ),
+            Spacer(),
+            GestureDetector(
+              onTap: () {
+                Razorpay razorpay = Razorpay();
+                var options = {
+                  'key': 'rzp_live_ILgsfZCZoFIKMb',
+                  'amount': cartProvider.totalPrice * 100,
+                  'name': 'Acme Corp.',
+                  'description': 'Fine T-Shirt',
+                  'retry': {'enabled': true, 'max_count': 1},
+                  'send_sms_hash': true,
+                  'prefill': {
+                    'contact': '8888888888',
+                    'email': 'test@razorpay.com'
+                  },
+                  'external': {
+                    'wallets': ['paytm']
+                  }
+                };
+                razorpay.on(
+                    Razorpay.EVENT_PAYMENT_ERROR, handlePaymentErrorResponse);
+                razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS,
+                    handlePaymentSuccessResponse);
+                razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET,
+                    handleExternalWalletSelected);
+                razorpay.open(options);
+              },
+              child: Container(
+                height: 38,
+                padding: EdgeInsets.symmetric(horizontal: 17),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: ColorConstants.green,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Text(
+                  "Check Out",
+                  style: TextStyle(
+                    color: ColorConstants.white,
+                    fontSize: 19,
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: -0.6,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void handlePaymentErrorResponse(PaymentFailureResponse response) {
+    /*
+    * PaymentFailureResponse contains three values:
+    * 1. Error Code
+    * 2. Error Description
+    * 3. Metadata
+    * */
+    showAlertDialog(context, "Payment Failed",
+        "Code: ${response.code}\nDescription: ${response.message}\nMetadata:${response.error.toString()}");
+  }
+
+  void handlePaymentSuccessResponse(PaymentSuccessResponse response) {
+    /*
+    * Payment Success Response contains three values:
+    * 1. Order ID
+    * 2. Payment ID
+    * 3. Signature
+    * */
+    showAlertDialog(
+        context, "Payment Successful", "Payment ID: ${response.paymentId}");
+  }
+
+  void handleExternalWalletSelected(ExternalWalletResponse response) {
+    showAlertDialog(
+        context, "External Wallet Selected", "${response.walletName}");
+  }
+
+  void showAlertDialog(BuildContext context, String title, String message) {
+    // set up the buttons
+    Widget continueButton = ElevatedButton(
+      child: const Text("Continue"),
+      onPressed: () {},
+    );
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text(title),
+      content: Text(message),
+    );
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
     );
   }
 }
